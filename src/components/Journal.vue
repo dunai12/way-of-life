69a264d07c739b4d4ff12cd18b8bcd878a05c56d
Dunai <dunai12@mail.ru>
2018-03-22 20:33:33 +0300

    mend

---
+++ src/components/Journal.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="journal">
+        <div class="journal__header">
+            <div class="journal__header-top">
+                <div class="journal__change">Change</div>
+                <div class="journal__progress-line">
+                    <progless-line/>
+                </div>
+                <div class="journal__add">+</div>
+            </div>
+        </div>
+        <div class="journal__content"></div>
+    </div>
+</template>
+
+<script>
+import proglessLine from './ProglessLine.vue';
+export default {
+    components: {
+        proglessLine
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/rootComponents/NavMenu.vue
@@ -0,0 +1,82 @@
+<template>
+    <ul class="nav-menu">
+        <router-link
+            v-for="(item, index) in items"
+            :key="index"
+            tag="li"
+            class="nav-menu__item"
+            active-class="nav-menu__item_active"
+            :to="item.route">
+            <span
+                class="nav-menu__icon fas"
+                :class="item.icon">
+            </span>
+            {{item.name}}
+        </router-link>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    name: 'Журнал',
+                    icon: 'fa-list-alt',
+                    route: '/journal'
+                },
+                {
+                    name: 'Графики',
+                    icon: 'fa-signal',
+                    route: '/statistics'
+                },
+                {
+                    name: 'Заметки',
+                    icon: 'fa-sticky-note',
+                    route: '/notes'
+                },
+                {
+                    name: 'Напоминания',
+                    icon: 'fa-bell',
+                    route: '/notifications'
+                },
+                {
+                    name: 'Настройки',
+                    icon: 'fa-cog',
+                    route: '/settings'
+                }
+            ]
+        };
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+    @import "~scss/vars";
+
+    .nav-menu{
+        width: 100%;
+        display: flex;
+        padding: 0;
+        margin: 0;
+        list-style: none;
+        border-top: 1px solid $gray;
+        align-self: flex-end;
+
+        &__item{
+            color: $gray;
+            flex: 1 1 auto;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            cursor: pointer;
+            padding: 10px 0 5px;
+            font-size: 11px;
+        }
+        &__icon{
+            font-size: 16px;
+            margin-bottom: 5px;
+        }
+    }
+</style>
